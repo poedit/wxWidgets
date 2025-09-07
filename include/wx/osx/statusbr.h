085f9776d32aa38e2c6beee4c4a2f1a8bfdfe9b1
Václav Slavík <vaclav@slavik.io>
2025-09-07 09:48:36 +0200

    Don't put wxStatusBar fields into corners on macOS
    macOS 26 (and to lesser extent, macOS 11+) uses rounded corners for
    windows and status bar fields were rendered into the corner.
    
    Improve the appearance by offsetting the fields are appropriately to
    look better. Also add a way to customize the offset from user code,
    because the default could be unsuitable when e.g. the window uses larger
    corner radius (possible on macOS 26), or when user code wants to do
    smarter placement of the fields to mimic Finder (which centers the
    statusbar, but not under the entire window, but under the main area sans
    the sidebar).

--- include/wx/osx/statusbr.h
+++ include/wx/osx/statusbr.h
@@ -29,13 +29,22 @@ public:
     // Implementation
     virtual void MacHiliteChanged() wxOVERRIDE;
     void OnPaint(wxPaintEvent& event);
+    virtual bool GetFieldRect(int i, wxRect& rect) const override;
+
+    void MacSetCornerInset(int inset);
+    int MacGetCornerInset() const { return m_cornerInset; }
 
 protected:
     virtual int GetEffectiveFieldStyle(int WXUNUSED(i)) const wxOVERRIDE { return wxSB_NORMAL; }
 
     virtual void InitColours() wxOVERRIDE;
 
+    void InitCornerInset();
+
+    virtual int GetAvailableWidthForFields(int width) const override;
+
 private:
+    int m_cornerInset;
     wxColour m_textActive, m_textInactive, m_bgActive, m_bgInactive, m_separator;
 
     wxDECLARE_DYNAMIC_CLASS(wxStatusBarMac);
